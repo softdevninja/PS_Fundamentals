e08119e096f603f041eae6881b4961b9898e3d2f
Doug Zuniga <softdevninja@gmail.com>
2026-01-03 21:15:26 -0500

    Started taking notes and testing script.

--- DataTypes/src/index.js
+++ DataTypes/src/index.js
@@ -1,1 +1,16 @@
-//Entry point
+/**
+ *  JavasScript Engines
+ *  - V8 (Google Chrome, Microsoft Edge)
+ *  - JavaScript Core (Safari)
+ *  - SpiderMonkey (Firefox)
+ *  - Node.js (Server and Local Computer)
+ * 
+ *  LTS -> Long term support
+ * 
+ *  Documentation: developer.mozilla.org/en-US/docs/Web/JavaScript
+ */
+
+let firstName = "Doug";
+let lastName = "Z";
+
+console.log(`Hello ${firstName} ${lastName}`);
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## PS_Fundamentals
 
 ### Videos contains multiple sections which will be broken down into directories for learning purporses. 
+
+## Section
+### 1 - Syntax, Variables, and Data Types
